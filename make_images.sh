b8bd35be0beeadd9d1db8767350a46a8aff00345
David Sanford <adraximezrinel@gmail.com>
2017-05-09 14:37:16 -0700

    Updates pyspark-mongo names

--- make_images.sh
+++ make_images.sh
@@ -1,5 +1,5 @@
 docker run --name mongo-tweet-store -v /Users/dsanford/DSI_Projects/Capstone/Tweet-Data:/data/db -d -p 27017:27017 mongo
-docker run --name pyspark-engine -d -p 8888:8888 --link mongo-tweet-store:mongo -v /Users/dsanford/DSI_Projects/Capstone:/home/jovyan/work pyspark_mongo
+docker run --name pyspark-engine -d -p 8888:8888 --link mongo-tweet-store:mongo -v /Users/dsanford/DSI_Projects/Capstone:/home/jovyan/work pyspark-mongo
 
 # to connect
 #from os import environ
