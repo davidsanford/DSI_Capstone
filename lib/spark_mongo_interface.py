b19c323002f46f30b96cf42603e012282f4b7ab9
Ubuntu <ubuntu@ip-172-31-25-168.us-west-1.compute.internal>
2017-05-16 21:10:21 +0000

    Updated tweet loading

--- lib/spark_mongo_interface.py
+++ lib/spark_mongo_interface.py
@@ -2,8 +2,8 @@ import pandas as pd
 from os import environ
 from pyspark.sql import SparkSession
 
-def load_tweets_from_mongo(collection = "unfiltered_tweets"):
-    location = "mongodb://"+environ['MONGO_PORT_27017_TCP_ADDR']+":27017/"+collection+".tweets"
+def load_tweets_from_mongo(ip = environ['MONGO_PORT_27017_TCP_ADDR'], collection = "unfiltered_tweets"):
+    location = "mongodb://"+ip+":27017/"+collection+".tweets"
 
     spark_session = (SparkSession
                      .builder
@@ -15,14 +15,16 @@ def load_tweets_from_mongo(collection = "unfiltered_tweets"):
     tweets_spark = \
         spark_session.read.format("com.mongodb.spark.sql.DefaultSource").load()
     tweets_spark = tweets_spark[['text','lang']]
-
+    tweets_spark = tweets_spark.filter(tweets_spark["lang"]=="en")
+    tweets_spark = tweets_spark[['text']].dropna()
+    
     tweets_pandas = pd.DataFrame(tweets_spark.collect())
     tweets_pandas.columns = tweets_spark.columns
 
-    tweets_pandas.dropna(inplace=True)
+    #tweets_pandas.dropna(inplace=True)
 
-    tweets_pandas = \
-        tweets_pandas[tweets_pandas["lang"] == "en"][["text"]].dropna()
+    #tweets_pandas = \
+    #    tweets_pandas[tweets_pandas["lang"] == "en"][["text"]].dropna()
 
     spark_session.stop()
     
