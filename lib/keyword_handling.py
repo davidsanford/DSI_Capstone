cd5e99230fa900ddcdf9d1792c3f7a8fdf553ec3
Ubuntu <ubuntu@ip-172-31-25-168.us-west-1.compute.internal>
2017-05-21 22:31:42 +0000

    Final updates from AWS instance

--- lib/keyword_handling.py
+++ lib/keyword_handling.py
@@ -15,7 +15,7 @@ def restrict_by_keywords(tweets_df, num_keywords, keyword_list = []):
     tweets_df = \
         tweets_df[tweets_df["keyword_matches"].apply(len) == num_keywords]
     tweets_df["keyword_matches"] = \
-        tweets_df["keyword_matches"].apply(lambda x: x[0] if len(x) > 0 \
+        tweets_df["keyword_matches"].apply(lambda x: x.loc[0] if len(x) > 0 \
                                            else np.nan)
     
     return tweets_df
